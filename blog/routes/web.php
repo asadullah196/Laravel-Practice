3a9bec825cbc4a92f5c85559d76f0e54ea425c0e
Asadullah <md.asadullah196@gmail.com>
2020-10-27 22:51:27 +0600

    View Customization Practice

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -13,15 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/','App\Http\Controllers\SiteController@Home');
+//Route::get('/','App\Http\Controllers\SiteController@Home');
 
 Route::get('/about','App\Http\Controllers\SiteController@About');
 
 Route::get('/contact','App\Http\Controllers\SiteController@Contact');
 
-// Route::get('/',function(){
-//     return view('GreensoftHome');
-// });
+Route::get('/',function(){
+    return view('welcome');
+});
 
 // Route::get('/', function () {
 //     return view('welcome');
